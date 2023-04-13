820b7012b4b1e1f27f7ede116bdd8f5a0c233e4d
Yulia Osnova <yulia.osnova@gmail.com>
2023-04-13 17:03:04 +0300

    fix

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -17,6 +17,7 @@ function Modal({ children }) {
       //   console.log("Unmounting phase: same when componentWillUnmount runs");
       window.removeEventListener('keydown', handleKeyDown);
     };
+  // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   const handleKeyDown = e => {
